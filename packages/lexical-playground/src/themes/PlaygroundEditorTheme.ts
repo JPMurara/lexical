fb9a1bb8496f7cd00bb0a85c9645fcb4a31756cf
Joao Murara <joaopaulo.murara@gmail.com>
2024-11-08 08:47:07 +1000

    css fix

--- packages/lexical-playground/src/themes/PlaygroundEditorTheme.ts
+++ packages/lexical-playground/src/themes/PlaygroundEditorTheme.ts
@@ -11,6 +11,7 @@ import type {EditorThemeClasses} from 'lexical';
 import './PlaygroundEditorTheme.css';
 
 const theme: EditorThemeClasses = {
+  DXPImage: 'inline-editor-image',
   autocomplete: 'PlaygroundEditorTheme__autocomplete',
   blockCursor: 'PlaygroundEditorTheme__blockCursor',
   characterLimit: 'PlaygroundEditorTheme__characterLimit',
